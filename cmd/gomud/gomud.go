42f60f1a0a4917154407afcf1e497bc6b0195062
Antonio Lobato <aj.lobato@gmail.com>
2021-07-05 23:02:25 +0000

    added debug server

--- cmd/gomud/gomud.go
+++ cmd/gomud/gomud.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"net/http"
+	_ "net/http/pprof"
+
 	"github.com/Cidan/gomud/construct"
 	"github.com/Cidan/gomud/server"
 	"github.com/rs/zerolog/log"
@@ -18,11 +21,20 @@ func main() {
 	}
 
 	server := server.New()
+	go startDebugServer()
+
 	log.Info().Msg("Gomud listening on port 4000.")
 	if err := server.Listen(4000); err != nil {
 		log.Panic().Err(err).Msg("Error while listening for new connections.")
 	}
 	log.Info().Msg("Server shutting down.")
+}
+
+func startDebugServer() {
+	err := http.ListenAndServe(":8472", nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func makeDefaultRoomSet() {
