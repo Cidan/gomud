ca095f7c42c4f2194eca977abb6acd18c4ffc0ed
Antonio Lobato <aj.lobato@gmail.com>
2021-07-10 07:29:56 +0000

    fixed flaky test due to mock server not closing.

--- construct/room_test.go
+++ construct/room_test.go
@@ -2,7 +2,6 @@ package construct
 
 import (
 	"bufio"
-	"fmt"
 	"net"
 	"testing"
 
@@ -27,27 +26,27 @@ func LogPlayerIn(t *testing.T, reader *bufio.Reader, writer *bufio.Writer) {
 	for _, command := range loginCommands {
 		writer.WriteString(command + "\n")
 		writer.Flush()
-		repl, err := reader.ReadString('\r')
+		_, err := reader.ReadString('\r')
 		assert.Nil(t, err)
-		fmt.Printf("%s\n", repl)
 	}
 }
 
 // This test rapidly moves a player between two rooms, while deleting a room in
 // another go routine, ensuring room deletes are concurrently safe.
+// This test should be run with race detection via `go test -race -count=3`
 func TestPlayerMovementRace(t *testing.T) {
 	config.Set("save_path", t.TempDir())
 	makeStartingRoom()
 	p := NewPlayer()
 	assert.NotNil(t, p)
 	server := server.New()
-	go server.Listen(2000, func(c net.Conn) {
+	go server.Listen(2020, func(c net.Conn) {
 		// Simulated player connection loop
 		p.SetConnection(c)
 		go p.Start()
 	})
 
-	conn, err := net.Dial("tcp", "localhost:2000")
+	conn, err := net.Dial("tcp", "localhost:2020")
 	assert.Nil(t, err)
 	reader := bufio.NewReader(conn)
 	writer := bufio.NewWriter(conn)
@@ -75,4 +74,5 @@ func TestPlayerMovementRace(t *testing.T) {
 		}
 	}
 	<-c
+	server.Close()
 }
